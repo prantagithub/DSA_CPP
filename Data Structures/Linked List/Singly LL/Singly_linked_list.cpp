e662265e43669233dd974c8ee986e71f36297f72
prantagithub <62611184+prantagithub@users.noreply.github.com>
2021-03-27 11:08:31 +0530

    updated to add more methods (not tested)

--- Data Structures/Linked List/Singly LL/Singly_linked_list.cpp
+++ Data Structures/Linked List/Singly LL/Singly_linked_list.cpp
@@ -1,5 +1,8 @@
 //Singly linked list
 #include <iostream>
+#include <pair>
+#include <stack>
+#include <unordered_map>
 using namespace std;
 //-----------------------Node-------------------
 struct Node{
@@ -117,6 +120,146 @@ public:
 	}
 
 };
+//-------function to find length of a given array iterative-----------
+long long lengthIter(Node* root){
+	long long l=0;
+	while(root!= NULL){
+		l++;
+		root=root->next;
+	}
+}
+//-------------recursive length solution------------------------
+long long lengthRec(Node* root){
+	if(root==NULL) return 0;
+	else{
+		return 1+lengthRec(root->next);
+	}
+}
+//-------------------Detect loop---------------
+int detectLoop(Node* root){
+	int len=0;
+	unordered_map<Node*, pair<bool,int>> mp;
+	int count=0;
+	bool val=false;
+	Node* temp=root;
+	while(temp!=NULL){
+		if(mp[temp].first==false) {
+			mp[temp].first=true;
+			mp[temp].second=count;
+		}
+		else{
+			val=true;
+			break;
+		}
+		temp=temp->next;
+		count++;
+	}
+	if(val==false) return 0;
+	else{
+		return count+1-mp[temp].second;
+	}
+}
+//-------------------ifPalindrome check--------------------------
+bool isPalindrome(Node* root,int n){
+	stack st;
+	Node* temp=root;
+	for(int i=0;i<n;i++){
+		if(i<n/2) st.push_back(temp->value);
+		else{
+			if(n%2==0){
+				if(temp->value!=st.top()) return false;
+				else st.pop();
+			}
+			else if(i>n/2){
+				if(temp->value!=st.top()) return false;
+				else st.pop();
+			}
+		}
+		temp=temp->next;
+	}
+	return true;
+}
+//-----------------find duplicates from sorted LL------------------
+Node*  remoDupSorted(Node* root){
+	Node* temp=root;
+	while(temp->next!=NULL){
+		if(temp->next->value==temp->value){
+			Node* t=temp->next;
+			temp->next=t->next;
+		}
+		if(temp->next->value!=temp->value)
+		{
+			temp=temp->next;
+		}
+	}
+	return temp;
+}
+//---------------remove duplicate in an unsorted LL-------------------------
+Node* remDupUnsort(Node* root){
+	Node* temp=root->next;
+	unordered_map<int, bool> mp;
+	Node* prev=root;
+	while(temp!=NULL){
+		if(mp[temp->value]!=false){
+			prev->next=temp->next;
+		}
+		else{
+			mp[temp->value]=true;
+			prev=temp;
+			temp=temp->next;
+		}
+
+	}
+	return temp;
+}
+//----------------Node swapping without value swapping----------------------
+Node* swapNode(Node* root,int pos1,int pos2,int length){
+	Node* temp=root;
+	Node* n1,n2,p1,p2,ne1,ne2;
+	for(int i=0;i<length;i++){
+		if(i==pos1-1){
+			p1=temp;
+			n1=temp->next;
+			ne1=n1->next;
+		}
+		if(i==pos2-1){
+			p2=temp;
+			n2=temp->next;
+			ne2=n1->next;
+		}
+		temp=temp->next;
+	}
+	//swap
+	p1->next=n2;
+	n2->next=ne1;
+	p2->next=n1;
+	n1->next=ne2;
+	return temp;
+}
+//-----------move last element to front-----------
+Node* movToFront(Node* root,int length){
+	Node* temp=root;
+	Node* head=root;
+	for(int i=0;i<length-1;i++){
+		temp=temp->next;
+	}
+	Node* res=temp->next->next;
+	res=head;
+	temp->next=NULL;
+	return res; 
+}
+//---------------reverse a linked list--------------
+Node* reverse(Node* root){
+	Node* temp=root,prev=NULL,prev2;
+	while(temp!=NULL){
+		prev2=temp->next;
+		temp->next=prev;
+		prev=temp;
+		temp=prev2;
+	}
+	return prev;
+}
+//------------------END--------------------------
 //----------------------------------------------
 int main(){
 	//-----------code--------------------------
